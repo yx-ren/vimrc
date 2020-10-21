aa776c582c22d70136a2b1fc1d3cbd2571ba3424
yx-ren <yunxiang.r@gmail.com>
2020-10-22 01:01:23 +0800

    bakup origin vim config files

--- build.sh
+++ build.sh
@@ -1,3 +1,20 @@
 #/bin/bash
+
+HOME_DIR=$(echo ~)
+
+BAK_POSTFIX=$(date +"%Y%m%d-%H%M%S")
+BAK_DIR="/tmp/$BAK_POSTFIX.vimbak/"
+mkdir $BAK_DIR
+echo "create bak dir:[$BAK_DIR]"
+
+BAK_FILES=("$HOME_DIR/.vimrc" "$HOME_DIR/.vim/")
+for ((i = 0; i != ${#BAK_FILES[@]}; i++))
+do
+    BAK_SRC=${BAK_FILES[i]}
+    if [ -d $BAK_SRC ] || [ -f $BAK_SRC ]; then
+        echo "backup, move [$BAK_SRC] -> [$BAK_DIR]"
+        mv "$BAK_SRC" "$BAK_DIR"
+    fi
+done
+
 ./copy_config.sh
-#source ./install.sh
--- copy_config.sh
+++ copy_config.sh
@@ -3,11 +3,9 @@
 HOME_DIR=$(echo ~)
 
 VIMRC_PATH="$HOME_DIR/.vimrc"
-rm -rf $VIMRC_PATH
 cp ./.vimrc $VIMRC_PATH
 
 VIM_AUTOLOAD_DIR="$HOME_DIR/.vim/autoload"
-rm -rf $VIM_AUTOLOAD_DIR
 mkdir -p $VIM_AUTOLOAD_DIR
 
 VIM_PLUG_PATH="./plug.vim"
